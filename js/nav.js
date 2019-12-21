19c0805d73cbbeadd979e4b23ac15dc1fda5ecab
Nabeel Warsalee <nabeel.warsalee@gmail.com>
2019-12-21 12:58:31 -0500

    Horizontal navigation bar
    Changed the navigation bar so that it is horizontal instead of vertical.
    Also changed the icon to exit out of the menu to an up arrow insted of an x.

--- js/nav.js
+++ js/nav.js
@@ -3,11 +3,9 @@ const nav = document.querySelector('nav');
 const containerAll = document.querySelector('.container_all');
 
 nav_toggle.addEventListener('click', ()=> {
-    containerAll.style.transition = 'transform 500ms ease-in-out';
     document.body.classList.toggle("nav_is_open");
 });
 
 nav.addEventListener('click', ()=> {
-    containerAll.style.transition = '0ms';
     document.body.classList.remove('nav_is_open');
 });
